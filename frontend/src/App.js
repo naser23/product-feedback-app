eacb8d01838e95e524ac06dc38904e1a220cb400
naser23 <naserabdulkadir0@gmail.com>
2022-09-06 17:02:44 -0400

    Working on setting up frontend stylesheets rn. Will continue working on that.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,24 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Home from "./pages/Home";
+import "./css/main.css";
 
 function App() {
-  return <div className="App">My App</div>;
+  return (
+    <>
+      <Router>
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </div>
+      </Router>
+    </>
+  );
 }
 
 export default App;
---
+++ frontend/src/pages/Login.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Login() {
+  return <main className="mainPage">login</main>;
+}
+
+export default Login;
