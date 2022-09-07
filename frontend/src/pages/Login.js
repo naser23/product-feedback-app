95827f94125b086da8bb56555a00b38f7483ba9e
naser23 <naserabdulkadir0@gmail.com>
2022-09-07 15:46:02 -0400

    Working on Login page UI. Will also work on registar page and home page UI after.

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,7 +1,29 @@
 import React from "react";
 
 function Login() {
-  return <main className="mainPage">login</main>;
+  return (
+    <main className="login">
+      <div className="login-container">
+        <h1>Login</h1>
+        <form className="form-control">
+          <input
+            className="input-field"
+            type="text"
+            id="email"
+            placeholder="Enter your email"
+          />
+          <input
+            className="input-field"
+            type="password"
+            id="password"
+            placeholder="Enter your password"
+          />
+          <button className="submit">Login to your account</button>
+          <p>Dont have an account? Sign up here!</p>
+        </form>
+      </div>
+    </main>
+  );
 }
 
 export default Login;
