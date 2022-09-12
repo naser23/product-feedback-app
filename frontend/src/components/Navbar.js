601fd558873c8b2f52bd8e663cd10d9a25cada37
naser23 <naserabdulkadir0@gmail.com>
2022-09-12 14:05:52 -0400

    Working on homepage Header now. Will continue working on it.

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,12 +1,15 @@
 import React from "react";
+import { faLogin } from "react-icons/fa";
 
 function Navbar() {
   return (
     <header className="navbar">
       <section className="logoArea">
-        <h2 className="logoHeader">Frontend Mentor</h2>
+        <h3 className="logoHeader">Frontend Mentor</h3>
         <p className="logoSubHeader">Feedback Board</p>
       </section>
+      <section className="categoryArea"></section>
+      <section className="RoadmapArea"></section>
     </header>
   );
 }
