8253b163ac08cf4230e95a53feac78a50a97bd63
naser23 <naserabdulkadir0@gmail.com>
2022-09-15 16:31:20 -0400

    Working on the desktop and tablet navbar roadmap component. I need to clean up the code in roadmap and find a way to get the same design with just li components. It looks really messy.

--- frontend/src/components/Roadmap.js
+++ frontend/src/components/Roadmap.js
@@ -1,7 +1,37 @@
 import React from "react";
 
 function Roadmap() {
-  return <section className="roadmapArea">RoadMap</section>;
+  return (
+    <section className="roadmapArea">
+      <div className="roadmapHeader">
+        <h3>Roadmap</h3>
+        <p>View</p>
+      </div>
+
+      <div className="roadmapProgress">
+        <ul className="options">
+          <li className="option">
+            <p>
+              <span className="peachDot"></span> Planned
+            </p>
+            <span className="optionCount">2</span>
+          </li>
+          <li className="option">
+            <p>
+              <span className="purpleDot"></span>In-Progress
+            </p>
+            <span className="optionCount">3</span>
+          </li>
+          <li className="option">
+            <p>
+              <span className="blueDot"></span>Live
+            </p>
+            <span className="optionCount">1</span>
+          </li>
+        </ul>
+      </div>
+    </section>
+  );
 }
 
 export default Roadmap;
