bd3469951d3cc8d0d5767404a3466d4956b567d7
naser23 <naserabdulkadir0@gmail.com>
2022-09-15 20:45:35 -0400

    Still working on mobile Roadmap component. afterwards I will work on the rest of the homepage.

--- frontend/src/components/MobileNav.js
+++ frontend/src/components/MobileNav.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Roadmap from "./Roadmap";
 
 function MobileNav() {
   const categories = ["All", "UI", "UX", "Enhancement", "Bug", "Feature"];
@@ -14,7 +15,37 @@ function MobileNav() {
           ))}
         </div>
       </section>
-      <section className="roadmapAreaMobile">RoadMap</section>
+      <section className="roadmapAreaMobile">
+        <div className="roadmapHeader">
+          <h3>Roadmap</h3>
+          <p>View</p>
+        </div>
+        <div className="roadmapProgress">
+          <ul className="options">
+            <li className="option">
+              <div>
+                <span className="peachDot"></span>
+                <p>Planned</p>
+              </div>
+              <p className="optionCount">2</p>
+            </li>
+            <li className="option">
+              <div>
+                <span className="purpleDot"></span>
+                <p>In-Progress</p>
+              </div>
+              <span className="optionCount">3</span>
+            </li>
+            <li className="option">
+              <div>
+                <span className="blueDot"></span>
+                <p>Live</p>
+              </div>
+              <span className="optionCount">1</span>
+            </li>
+          </ul>
+        </div>
+      </section>
     </nav>
   );
 }
