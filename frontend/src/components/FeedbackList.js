e0456179995813859255f89ac1e7191b43bd4f5c
naser23 <naserabdulkadir0@gmail.com>
2022-09-19 18:08:08 -0400

    Still working on styling for FeedbackList and FeedbackListItems.

--- frontend/src/components/FeedbackList.js
+++ frontend/src/components/FeedbackList.js
@@ -1,7 +1,12 @@
 import React from "react";
+import FeedbackItem from "./FeedbackItem";
 
 function FeedbackList() {
-  return <section className="feedbackList"></section>;
+  return (
+    <section className="feedbackList">
+      <FeedbackItem />
+    </section>
+  );
 }
 
 export default FeedbackList;
