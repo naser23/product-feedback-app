6dcdf350bdfe2e2a11550436fc2c3edeaed0432d
naser23 <naserabdulkadir0@gmail.com>
2022-09-20 15:32:05 -0400

    Got FeedbackItem styling done. Will work on styling option if ther is no feedback Items and will work on Roadmap page. After those two are done then I can move over to the backend and get the feedback functionality started.

--- frontend/src/components/FeedbackItem.js
+++ frontend/src/components/FeedbackItem.js
@@ -1,7 +1,27 @@
 import React from "react";
+import UpvoteIcon from "../assets/shared/icon-arrow-up.svg";
+import CommentBubble from "../assets/shared/icon-comments.svg";
 
 function FeedbackItem() {
-  return <div className="feedbackItem">FeedbackItem</div>;
+  return (
+    <div className="feedbackItem">
+      <div className="upvotes">
+        <img src={UpvoteIcon} alt="UpvoteIcon" />
+        <p className="fontRegular">0</p>
+      </div>
+      <div className="feedbackText">
+        <h3 className="fontSemiBold">Suggestion Name</h3>
+        <p className="fontRegular feedbackDescription">
+          This is the suggestion text.
+        </p>
+        <p className="feedbackCategory fontSemiBold">Enhancement</p>
+      </div>
+      <div className="commentCount">
+        <img src={CommentBubble} alt="CommentBubble" />
+        <p className="fontSemiBold">2</p>
+      </div>
+    </div>
+  );
 }
 
 export default FeedbackItem;
