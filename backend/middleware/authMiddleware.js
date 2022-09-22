c04ee41f6ec052cd18b2db74a2b86b37e1f22a93
naser23 <naserabdulkadir0@gmail.com>
2022-09-21 21:48:57 -0400

    Fixed issue with authMiddleware. Needed to literally add a space inside the quotes in the split method. Will definitely need this for the comment backend when i get there but i want to see if i need to use this for the feedback suggestions as well.

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -10,7 +10,8 @@ const protect = asyncHandler(async (req, res, next) => {
     req.headers.authorization.startsWith("Bearer")
   ) {
     try {
-      token = req.headers.authorization.split("")[1];
+      token = req.headers.authorization.split(" ")[1];
+      console.log(`token: ${token}`);
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
       req.user = await User.findById(decoded.id).select("-password");
       next();
