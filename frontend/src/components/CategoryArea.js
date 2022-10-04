6080e3d8e7a31a9ad2b8bc4892aa111665a15834
naser23 <naserabdulkadir0@gmail.com>
2022-10-04 12:54:49 -0400

    Working on the functionality for sorting through suggestions from navbar.

--- frontend/src/components/CategoryArea.js
+++ frontend/src/components/CategoryArea.js
@@ -1,14 +1,24 @@
 import React from "react";
+import { useState } from "react";
 
 function CategoryArea() {
   const categories = ["All", "UI", "UX", "Enhancement", "Bug", "Feature"];
+
+  const [feedbackCategory, setFeedbackCategory] = useState("");
+  const [isActive, setIsActive] = useState(false);
+
   return (
     <section className="categoryArea">
       <div className="categories">
         {categories.map((category) => (
           <p
-            className="category fontSemiBold"
+            className={
+              feedbackCategory !== category
+                ? "category fontSemiBold"
+                : "active category fontSemiBold"
+            }
             key={categories.indexOf(category)}
+            onClick={() => setFeedbackCategory(category)}
           >
             {category}
           </p>
