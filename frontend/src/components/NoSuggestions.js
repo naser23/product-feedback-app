f86971605e134524edbc4bf81d8749b495c326e3
naser23 <naserabdulkadir0@gmail.com>
2022-10-05 13:51:50 -0400

    Got most of the styling of the NoSuggestions component but all of the functionality done for it. Might rework the styling to make it be more inline.

--- frontend/src/components/NoSuggestions.js
+++ frontend/src/components/NoSuggestions.js
@@ -1,7 +1,35 @@
 import React from "react";
+import IllustrationEmpty from "../assets/suggestions/illustration-empty.svg";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { reset } from "../features/feedback/feedbackSlice";
 
 function NoSuggestions() {
-  return <div>NoSuggestions</div>;
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  function resetAndNavigate() {
+    dispatch(reset());
+    navigate("/add-feedback");
+  }
+
+  return (
+    <section className="noFeedbackContainer">
+      <div className="noFeedback">
+        <img src={IllustrationEmpty} alt="No suggestions" />
+        <h1 className="noFeedbackHeader fontSemiBold">
+          There is no feedback yet
+        </h1>
+        <p className="noFeedbackText fontRegular">
+          Got a suggestion? Found a bug that needs to be squashed? We love
+          hearing about new ideas to improve our app.
+        </p>
+        <button onClick={resetAndNavigate} className="addFeedback fontSemiBold">
+          + Add Feedback
+        </button>
+      </div>
+    </section>
+  );
 }
 
 export default NoSuggestions;
